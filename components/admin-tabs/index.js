be4095afa3342b8b6f0441c7fff559085f7cb856
Sander <sander@wieni.be>
2020-05-14 11:03:31 +0200

    Add admin tabs

---
+++ components/admin-tabs/index.js
@@ -0,0 +1,24 @@
+import Popper from "popper.js";
+
+const buttons = document.querySelectorAll('[data-toggle="dropdown"]');
+
+[...buttons].map(button => {
+  button.addEventListener("click", () => {
+    const menu = button.parentNode.querySelector(".dropdown-menu");
+    menu.classList.toggle("show");
+    // eslint-disable-next-line no-unused-vars
+    const pop = new Popper(button, menu, {
+      placement: "bottom-start",
+    });
+
+    const clickOff = (evt) => {
+      if (!button.contains(evt.target)) {
+        document.removeEventListener('click', clickOff);
+        menu.classList.toggle("show");
+      }
+     };
+     document.addEventListener('click', clickOff);
+  });
+
+  return null;
+});
