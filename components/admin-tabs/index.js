4188eae03762c4ff2ea490a9008b6ef83a2bf6b0
Sander <sander@wieni.be>
2020-05-15 09:07:43 +0200

    Code linting

--- components/admin-tabs/index.js
+++ components/admin-tabs/index.js
@@ -2,7 +2,7 @@ import Popper from "popper.js";
 
 const buttons = document.querySelectorAll('[data-toggle="dropdown"]');
 
-[...buttons].map(button => {
+[...buttons].map((button) => {
   button.addEventListener("click", () => {
     const menu = button.parentNode.querySelector(".dropdown-menu");
     menu.classList.toggle("show");
@@ -13,11 +13,12 @@ const buttons = document.querySelectorAll('[data-toggle="dropdown"]');
 
     const clickOff = (evt) => {
       if (!button.contains(evt.target)) {
-        document.removeEventListener('click', clickOff);
+        document.removeEventListener("click", clickOff);
         menu.classList.toggle("show");
       }
-     };
-     document.addEventListener('click', clickOff);
+    };
+
+    document.addEventListener("click", clickOff);
   });
 
   return null;
